5873f97223f3001336a300d8b80931fc6c5dfb6b
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2015-05-22 13:48:51 +0100

    Refactoring and fixing indents

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('webgl').appendChild(VIEW3D.renderer.domElement);
-var stats = new Stats();
-//document.getElementById('info').appendChild( stats.domElement );
-window.addEventListener( 'resize', function(){
-  VIEW3D.resize(window.innerWidth, window.innerHeight)}, false );
-
-mainLoop();
